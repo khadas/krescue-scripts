6f1845d3a6e9de6c28fecace218becd4a704b488
Artem Lapkin <art@khadas.com>
2021-09-25 15:31:07 +0800

    scripts/install/Fedora-Linux-install.sh: update
    Signed-off-by: Artem Lapkin <art@khadas.com>

--- scripts/install/Fedora-Linux-install.sh
+++ scripts/install/Fedora-Linux-install.sh
@@ -31,7 +31,21 @@
 # sudo cp -av /boot/dtb/amlogic  /boot/efi/dtb
 # + VIM3 VIM3L no sound kernel
 # # CONFIG_COMMON_CLK_AXG_AUDIO is not set
+#/boot/config-5.13.13-200.fc34.aarch64:# CONFIG_COMMON_CLK_AXG_AUDIO is not set
+#/boot/config-5.14.2-300.fc35.aarch64:# CONFIG_COMMON_CLK_AXG_AUDIO is not set
+#/boot/config-5.15.0-0.rc0.20210902git4ac6d90867a4.4.fc36.aarch64:# CONFIG_COMMON_CLK_AXG_AUDIO is not set
 # need rebuild kernel
+
+# kernels next update
+# https://fedoraproject.org/wiki/Test_Day:2021-09-12_Kernel_5.14_Test_Week
+# sudo dnf install koji
+# koji download-build --arch=aarch64 kernel-5.14.2-300.fc35
+# sudo dnf update kernel-*.rpm
+# or
+# koji download-build --arch=aarch64 kernel-5.15.0-0.rc0.20210902git4ac6d90867a4.4.fc36
+
+# tests
+# https://testdays.fedoraproject.org/events
 
 set -e -o pipefail
 
@@ -97,7 +111,8 @@ sync
 
 sfdisk --dump $DST | tee /tmp/parts.data
 partx -u $DST -v || true
-blkid
+blkid | tee /tmp/parts.type
+
 
 ## oops boot partition start at 2048 - its ugly and not enough for uboot ;-)
 #/dev/mmcblk1p1 : start=        2048, size=     1228800, type=6, bootable
@@ -106,12 +121,46 @@ blkid
 mkdir -p 1 2 3
 mount ${DST}p1 1
 mount ${DST}p2 2
-mount ${DST}p2 3
+
+B=/dev/mapper/fedora_fedora-root
+
+[ "$NO_MOD" ] || {
+echo "[i] rootfs modification ..."
+
+grep LVM2_member /tmp/parts.type && {
+which lvm >/dev/null 2>&1 || {
+modules_download_ipk dm
+echo "install lvm..."
+opkg_update && opkg install lvm2
+}
+vgchange -a y
+}
+
+ROOT=3
+if [ -b "$B" ]; then
+mount "$B" 3
+else
+mount ${DST}p3 3
+ROOT=3/root
+fi
+
+# copy wifi fw
+cp -a /lib/firmware/brcm $ROOT/lib/firmware
+
+# unlock root
+#chroot $ROOT passwd -d root
+
+# setup secure tty
+#echo ttyAML0 >> $ROOT/etc/securetty
+#echo ttyFIQ0 >> $ROOT/etc/securetty
+} # END rootfs mod
+
+# LOGO
+curl -A downloader -jkL http://dl.khadas.com/.dl/logos/fedora-logo.bmp.gz -o 1/splash.bmp
+
 mkdir -p 1/dtb
 # copy amlogic and rockhip dtb's
 cp -a 2/dtb/aml* 2/dtb/rock* 1/dtb
-# copy wifi fw
-cp -a /lib/firmware/brcm 3/root/lib/firmware
 
 # more verbose boot
 #sed -i "s/ rhgb quiet console=tty0//" 2/grub2/grub.cfg
@@ -125,14 +174,10 @@ for c in 2/loader/entries/*.conf; do
     cat $c
 done
 
-[ "" ] && {
-chmod 0777 system/etc/rc.local
-# setup secure tty
-echo ttyAML0 >> system/etc/securetty
-echo ttyFIQ0 >> system/etc/securetty
-}
+umount 1 2 3 || true
 
-umount 1 2 3
+#[ -b "$B" ] && vgchange -a n
+which vgchange 2>/dev/null && vgchange -a n
 
 #exit 0
 
